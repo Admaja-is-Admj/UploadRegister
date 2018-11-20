38d2483261f23bcb51d2b5ff7efefcccb6f3b939
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2018-11-20 19:20:26 +0700

    oke1.2

--- functions.php
+++ functions.php
@@ -117,4 +117,49 @@ function cari($keyword){
 
             return query($sql);
 }
+function registrasi($data) {
+    global $conn;
+
+    //stripcslashes digunakan untuk menghilangkan backslashes
+    $username = strtolower(stripcslashes($data['username']));
+
+    //mysqli_real_escape_string untuk memberikan perlindungan terhadap karakter-karakter unik (sql_injection)
+    //pada mysqli_real_escape_string terdapat 2 parameter
+    $password = mysqli_real_escape_string($conn, $data['password']);
+    $password2 = mysqli_real_escape_string($conn, $data['password2']);
+
+    // cek username sudah ada apa belum
+    $result = mysqli_query ($conn, "SELECT username FROM users WHERE username = '$username'");
+
+    // cek kondisi jika line 175 bernilai true maka cetak echo
+    if (mysqli_fetch_assoc($result)) {
+        echo "
+            <script>
+                alert('username sudah ada');
+            </script>
+        ";
+        // agar proses insertnya gagal
+        return false;
+    }
+
+    //cek konfirmasi password
+    if ($password !== $password2) {
+        echo "
+            <script>
+                alert('password anda tidak sama');
+            </script>
+        ";
+        return false;
+    }
+
+// enkripsi password
+    $password = md5($password);
+    // $password = password_hash($password, PASSWORD_DEFAULT);
+    var_dump($password);
+
+// tambahkan user baru ke database
+mysqli_query($conn, "INSERT INTO users VALUES ('', '$username', '$password')");
+
+return mysqli_affected_rows($conn);
+}
 ?>
