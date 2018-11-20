176a0f742b2e55fc9820b0bcde71cd27d2ca422b
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2018-11-20 13:13:24 +0700

    Update tambah_data.php

--- tambah_data.php
+++ tambah_data.php
@@ -1,23 +1,9 @@
 <?php
     require 'functions.php';
-
-    if(isset($_POST['submit'])){
-        if(tambah($_POST) > 0){
-            echo "
-                <script>
-                    alert('data berhasil disimpan');
-                    document.location.href='index.php';
-                </script>
-                ";
-        } else {
-            echo "
-            <script>
-                alert('data gagal disimpan');
-                document.location.href='index.php';
-            </script>";
-            echo "<br>";
-            echo mysqli_error($conn);
-        }
+    if (isset($_POST['submit'])) {
+        var_dump($_POST);
+        var_dump($_FILES);
+        die();
     }
 ?>
 
@@ -27,36 +13,35 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         <title>Tambah data</title>
     </head>
     <body>
-        <h1 class="text-center">Tambah Data Mahasiswa</h1>
-        <form action="" method="post">
-            <ul>
-                <li>
-                    <label for="Nama">Nama:</label>
-                    <input type="text" name="Nama" id="Nama" required>
-                </li>
-                <li>
-                    <label for="Nim">NIM:</label>
-                    <input type="text" name="Nim" id="Nim" required>
-                </li>
-                <li>
-                    <label for="Email">Email</label>
-                    <input type="text" name="Email" id="Email" required>
-                </li>
-                <li>
-                    <label for="Jurusan">Jurusan</label>
-                    <input type="text" name="Jurusan" id="Jurusan" required>
-                </li>
-                <li>
-                    <label for="Gambar">Gambar</label>
-                    <input type="text" name="Gambar" id="Gambar" required>
-                </li>
-                <li>
-                    <button type="submit" name="submit"> Tambah </button>
-                </li>
-            </ul>
+        <form action="" method="POST" role="form">
+            <legend class="text-center">Tambah data mahasiswa</legend>
+                <div class="form-group">
+                <label for="Nama">Nama:</label>
+                <input type="text" class="form-control" id="" placeholder="Input field">
+                </div>
+                <div class="form-group">
+                <label for="Nim">NIM: </label>
+                <input type="text" class="form-control" id="" placeholder="input field">
+                </div>
+                <div class="form-group">
+                <label for="Email">Email: </label>
+                <input type="email" name="" id="input" class="form-control" value="" required="required" title="">
+                </div>
+                <div class="form-group">
+                <label for="Jurusan">Jurusan : </label>
+                <input type="text" class="form-control" id="" placeholder="input field">
+                </div>
+                <div class="form-group">
+                <label for="Gambar">Gambar : </label>
+                <input type="text" class="form-control" id="" placeholder="input field">
+                </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </body>
 </html>
